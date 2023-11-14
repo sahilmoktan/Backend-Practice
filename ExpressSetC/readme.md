2116e2c6748dfb27e0158894476a7f4c206c7149
sahilmoktan <moktanshail9@gmail.com>
2023-11-14 13:12:20 +0545

    flash message store and shown

--- ExpressSetC/readme.md
+++ ExpressSetC/readme.md
@@ -1,3 +1,7 @@
 flash message: npm i connect-flash
 don't forget to set session: npm i express-session
 put connect flash in  app.use function
+
+bootstrap cdn index shown in ejs file. put an error aleart message also from bootstrap
+abo hami /route ma jada error auuda flash message le respond gareko error agi ko bootstrap ko aleret ma auune banaune xau
+    flash message vanaale serverko katai route ma message banayera rakhne ani aru route ma show garne
--- ExpressSetC/routes/index.js
+++ ExpressSetC/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res) {
+  res.render("index");
+});
+
+/* GET login page. */
+router.get("/login", function (req, res) {
+  // login vayepaxi profile page show gardinu
+  // yedi login navaye, yo route bata aru route ma lanu like /error to show fail
+  // flash message allow u to see error all over other routes too
+  req.flash("errorName", 'errorData');
+  req.flash("age", 12);
+  res.send('error stored')
+});
+
+router.get("/check", function (req, res) {
+  console.log(req.flash("errorName"), req.flash('age'));
+  res.send('error in cansole')
+
 });
 
 module.exports = router;
